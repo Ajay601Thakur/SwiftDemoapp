1423cd42bbc97ff86c17952108fee35325161a03
ajay <athakur220221@gmail.com>
2022-07-13 16:30:37 +0530

    add Functions

---
+++ Example/PiaCharts/PieCharts.swift
@@ -0,0 +1,22 @@
+//
+//  PieCharts.swift
+//  SwiftDemoAppp_Example
+//
+//  Created by Apple on 13/07/22.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import Charts
+import UIKit
+
+
+class ChatManager:NSObject {
+    
+    func getSomeStringData()-> String {
+        return "No description, website, or topics provided"
+    }
+}
+
+
+
--- Example/SwiftDemoAppp/ViewController.swift
+++ Example/SwiftDemoAppp/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Charts
+import SwiftDemoAppp
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var subView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.updateChartData()
+        let title = ChatManager().getSomeStringData()
         
-        print("hello World")
-        // Do any additional setup after loading the view, typically from a nib.
+        print("title>>>>>\(title)")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateChartData()  {
+        let chart = PieChartView(frame: subView.frame)
+        // 2. generate chart data entries
+        let track = ["Income", "Expense", "Wallet", "Bank"]
+        let money = [650, 456.13, 78.67, 856.52]
+
+        var entries = [PieChartDataEntry]()
+        for (index, value) in money.enumerated() {
+            let entry = PieChartDataEntry()
+            entry.y = value
+            entry.label = track[index]
+            entries.append( entry)
+        }
+
+        // 3. chart setup
+        let set = PieChartDataSet( entries: entries, label: "Line Chart")
+        // this is custom extension method. Download the code for more details.
+        var colors: [UIColor] = []
+
+        for _ in 0..<money.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        set.colors = colors
+        let data = PieChartData(dataSet: set)
+        chart.data = data
+        chart.noDataText = "No data available"
+        // user interaction
+        chart.isUserInteractionEnabled = true
+
+        let d = Description()
+        chart.chartDescription = d
+        chart.holeRadiusPercent = 0.5
+        chart.transparentCircleColor = UIColor.clear
+        subView.addSubview(chart)
     }
 
 }
